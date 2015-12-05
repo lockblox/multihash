dd3fa7af26bda3ba6c302f3db649df06d682c887
jbrooker <jbrooker@users.noreply.github.com>
2015-12-05 11:28:08 +0000

    Initial version

---
+++ src/Type.cpp
@@ -0,0 +1,129 @@
+#include "Type.h"
+#include <algorithm>
+#include <sstream>
+
+namespace multihash
+{
+
+InvalidHashException::InvalidHashException(const std::string& message) 
+    : Exception(message) 
+{
+}
+
+HashType::Impl::Impl(HashCode code, std::string name, size_t size)
+        : code_(code), name_(std::move(name)), size_(size) 
+{ 
+}
+
+const HashType::Impl::ShpSet HashType::Impl::all_types_
+{
+    HashType::Impl::Shp{new HashType::Impl(HashCode::SHA1,    "sha1",    20)},
+    HashType::Impl::Shp{new HashType::Impl(HashCode::SHA2_256,"sha2-256",32)},
+    HashType::Impl::Shp{new HashType::Impl(HashCode::SHA2_512,"sha2-512",64)},
+    HashType::Impl::Shp{new HashType::Impl(HashCode::SHA3,    "sha3",    64)},
+    HashType::Impl::Shp{new HashType::Impl(HashCode::BLAKE2B, "blake2b", 64)},
+    HashType::Impl::Shp{new HashType::Impl(HashCode::BLAKE2S, "blake2s", 32)}
+};
+
+namespace
+{
+std::set<HashType> all_hash_types
+{
+    HashType{HashCode::SHA1},
+    HashType{HashCode::SHA2_256},
+    HashType{HashCode::SHA2_512},
+    HashType{HashCode::SHA3},
+    HashType{HashCode::BLAKE2B},
+    HashType{HashCode::BLAKE2S}
+};
+} // anonymous namespace
+
+HashType::HashType(const std::string &name)
+{
+    auto all_types(Impl::all_types());
+    auto end(std::end(all_types));
+    auto it(std::find_if(std::begin(all_types), end,
+                         [name] (const HashType::Impl::Shp& elem) -> bool
+                         { return elem && (name == elem->name()); }));
+    if (end == it) 
+    {
+        std::ostringstream err;
+        err << "Invalid hash name: " << name; 
+        throw InvalidHashException(err.str());
+    }
+    pImpl = *it;
+}
+
+HashType::HashType(HashCode code)
+{
+    auto all_types(Impl::all_types());
+    auto end(std::end(all_types));
+    auto it(std::find_if(std::begin(all_types), end,
+                         [code] (const HashType::Impl::Shp& elem) -> bool
+                         { return code == elem->code(); }));
+    if (end == it) 
+    {
+        std::ostringstream err;
+        err << "Invalid hash code: " << static_cast<int>(code); 
+        throw InvalidHashException(err.str());
+    }
+    pImpl = *it;
+}
+
+HashType::HashType(HashType&& rhs) noexcept : pImpl(std::move(rhs.pImpl))
+{
+    rhs.pImpl = nullptr;
+}
+
+HashType::HashType(const HashType& rhs) : pImpl(rhs.pImpl)
+{
+}
+
+const std::string& HashType::name() const
+{
+    return pImpl->name();
+}
+
+HashCode HashType::code() const
+{
+    return pImpl->code();
+}
+
+size_t HashType::size() const
+{
+    return pImpl->size();
+}
+
+const HashType::Impl::ShpSet& HashType::Impl::all_types()
+{
+    return all_types_;
+}
+
+bool HashType::operator==(const HashType& rhs) const
+{
+    return pImpl == rhs.pImpl;
+}
+
+bool HashType::operator<(const HashType& rhs) const
+{
+    return code() < rhs.code();
+}
+
+std::set<HashType> hashTypes()
+{
+    return all_hash_types;
+}
+
+} // namespace multihash
+
+std::ostream& operator<<(std::ostream& os, const multihash::HashType& hash_type)
+{
+    os << hash_type.name();
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const multihash::HashCode& hash_code)
+{
+    os << std::hex << static_cast<int>(hash_code);
+    return os;
+}
---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <multihash/multihash.h>
+#include <boost/program_options.hpp>
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#include <boost/filesystem.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[]) 
+{
+    // Declare the supported options.
+    std::ostringstream os;
+    os << "Usage: multihash [OPTION]... [FILE]...\n"
+       << "Print cryptographic digests.\n"
+       << "With no FILE or when file is -, read standard input";
+    po::options_description desc(os.str());
+    desc.add_options()
+        ("help", "display help message")
+        ("hash-type", po::value<std::string>(), "algorithm, e.g sha1")
+        ("list-hash-types", "list all available algorithms");
+
+    po::variables_map vm;
+
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << desc << std::endl;
+        return 1;
+    }    
+    if (vm.count("help") > 0) 
+    {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+    if (vm.count("list-hash-types") > 0)
+    {
+        for (auto hash_type : multihash::hashTypes())
+        {
+            std::cout << hash_type.name() << std::endl;
+        }
+        return 0;
+    }
+
+    /** Actually do some work if an algo is specified */
+    std::string algo;
+    if (vm.count("hash-type") == 1)
+    {
+        algo = vm["hash-type"].as<std::string>(); 
+    }
+    else
+    {
+        std::cerr << "Must specify a hash type" << std::endl;    
+        std::cerr << desc << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        auto hash_function = multihash::HashFunction(algo);
+
+        std::ios_base::sync_with_stdio(false); //enable fast io
+
+        std::vector<std::string> filenames;
+        if (argc > 3)
+        {
+            filenames.assign(&argv[3], &argv[argc-1]);
+        }
+        
+        auto num_files = filenames.size();
+        if ((num_files == 1 && (filenames.front() == "-")) or num_files == 0)
+        {
+            auto hash =  hash_function(std::cin);
+            std::cout << hash << " -" << std::endl; 
+        }
+        else
+        {
+            for (auto filename : filenames)
+            {
+                if (!boost::filesystem::exists(filename))
+                {
+                    std::cerr << "multihash: " << filename 
+                              << ": No such file or directory" << std::endl;
+                    continue;
+                }
+                else if (boost::filesystem::is_directory(filename))
+                {
+                    std::cerr << "multihash: " << filename
+                              << ": Is a directory" << std::endl;
+                    continue;
+                }
+                auto filestream = std::ifstream(filename);
+                if (filestream.good())
+                {
+                    auto hash = hash_function(filestream);
+                    std::cout << hash << " " << filename << std::endl;
+                }
+                else
+                {
+                    std::cerr << "multihash: " << filename << ": " 
+                              << "Permission denied" << std::endl;
+                }
+            }
+        }
+    }
+    catch (multihash::InvalidHashException& e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << "Available hash types: " << std::endl;
+        for (auto hash_type : multihash::hashTypes())
+        {
+            std::cout << hash_type.name() << std::endl;
+        }
+    }
+    return 0;
+}
