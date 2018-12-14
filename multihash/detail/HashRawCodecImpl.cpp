663b93430effc6336495b278e4321f2208a15281
markovchainy <markovchainy@users.noreply.github.com>
2018-12-14 18:52:54 +0000

    Throw exception if hash code or length are > 127 and therefore varint

--- multihash/detail/HashRawCodecImpl.cpp
+++ multihash/detail/HashRawCodecImpl.cpp
@@ -5,8 +5,15 @@
 namespace multihash {
 
 Hash HashRawCodec::Impl::decode(const std::vector<char>& raw_bytes) const {
-  auto code = static_cast<HashCode>((raw_bytes.at(0)));
+  auto code_byte = uint8_t(raw_bytes.at(0));
+  if (code_byte > 127) {
+    throw std::invalid_argument("Unsupported hash code > 127");
+  }
+  auto code = static_cast<HashCode>(code_byte);
   auto size = uint8_t(raw_bytes.at(1));
+  if (size > 127) {
+    throw std::invalid_argument("Unsupported digest size > 127");
+  }
   auto hash_type = HashType(code);
 
   std::vector<char> digest(begin(raw_bytes) + 2, end(raw_bytes));
@@ -14,13 +21,13 @@ Hash HashRawCodec::Impl::decode(const std::vector<char>& raw_bytes) const {
     std::ostringstream err;
     err << "Header misreports digest size as " << static_cast<int>(size)
         << "; true size is " << digest.size();
-    throw std::range_error(err.str());
+    throw std::out_of_range(err.str());
   }
   if (size != hash_type.size()) {
     std::ostringstream err;
     err << "Non standard digest size " << static_cast<int>(size)
         << "; expected " << hash_type.size();
-    throw std::range_error(err.str());
+    throw std::out_of_range(err.str());
   }
   return Hash(std::move(hash_type), digest);
 }
