4924da7603419a37af29da71c464c2cda265e1a6
markovchainy <markovchainy@users.noreply.github.com>
2021-06-19 19:52:29 +0100

    Added digest method to algorithm

--- multihash/algorithm.cpp
+++ multihash/algorithm.cpp
@@ -1,27 +1,10 @@
 #include <cryptopp/sha.h>
 #include <cryptopp/sha3.h>
 #include <multihash/algorithm.h>
-#include <multihash/code.h>
 #include <multihash/detail/cryptopp_impl.h>
 #include "detail/cryptopp_impl.h"
 
 namespace multihash {
-
-std::map<code_type, algorithm::factory*> algorithm::factories() {
-  static auto result = std::map<code_type, algorithm::factory*>{
-      {code::sha1, &detail::cryptopp_factory<CryptoPP::SHA1>::instance()},
-      {code::sha2_256, &detail::cryptopp_factory<CryptoPP::SHA256>::instance()},
-      {code::sha2_512, &detail::cryptopp_factory<CryptoPP::SHA512>::instance()},
-      {code::sha3_256,
-       &detail::cryptopp_factory<CryptoPP::SHA3_256>::instance()}};
-  return result;
-}
-
-std::unique_ptr<algorithm> algorithm::create(code_type code) {
-  auto factory = factories().at(code);
-  assert(factory);
-  return factory->create();
-}
 
 std::string algorithm::digest() {
   auto result = std::string(digest_size(), 0);
--- multihash/algorithm.h
+++ multihash/algorithm.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <multihash/code.h>
 #include <cstddef>
 #include <gsl/string_span>
 #include <map>
 #include <memory>
 #include <string_view>
+#include <vector>
 
 namespace multihash {
 
@@ -13,7 +13,18 @@ using string_span = gsl::span<char>;
 
 class algorithm {
  public:
+  /** Compute the hash of an input range */
+  template <typename InputIterator>
+  auto digest(InputIterator first, InputIterator last) -> std::string;
+
+  /** Write the hash of an input range into output */
+  template <typename InputIterator, typename OutputIterator>
+  auto digest(InputIterator first, InputIterator last,
+              OutputIterator output) -> OutputIterator;
+
   virtual ~algorithm() = default;
+
+ private:
   virtual void reset() = 0;
   virtual std::size_t digest_size() const = 0;
   virtual std::size_t block_size() const = 0;
@@ -21,16 +32,35 @@ class algorithm {
   virtual std::size_t digest(string_span output) = 0;
   std::string digest();
 
-  class factory {
-   public:
-    virtual ~factory() = default;
-    virtual std::unique_ptr<algorithm> create() = 0;
-  };
 
-  static std::unique_ptr<algorithm> create(code_type code);
-
- private:
-  static std::map<code_type, factory*> factories();
 };
+
+template <typename InputIterator>
+auto algorithm::digest(InputIterator first, InputIterator last) -> std::string {
+  auto result = std::string{};
+  result.reserve(digest_size());
+  digest(first, last, std::back_inserter(result));
+  return result;
+}
+
+template <typename InputIterator, typename OutputIterator>
+auto algorithm::digest(InputIterator first, InputIterator last,
+                                     OutputIterator output) -> OutputIterator {
+  reset();
+  auto buffer = std::vector<char>{};
+  auto chunk_size = block_size();
+  buffer.reserve(chunk_size);
+  while (first != last) {
+    for (auto i = 0u; first != last && i < chunk_size; ++i, ++first) {
+      buffer.emplace_back(*first);
+    }
+    auto view = std::string_view(buffer.data(), buffer.size());
+    update(view);
+    buffer.clear();
+  }
+  auto result = digest();
+  std::copy(result.begin(), result.end(), output);
+  return output;
+}
 
 }  // namespace multihash
