347366df516df98b810ef5d9bc10d314dcade314
markovchainy <markovchainy@users.noreply.github.com>
2021-06-19 20:04:11 +0100

    Changed algorithm_identifier from enum to unsigned int to conform with open/closed principle

--- multihash/algorithm_identifier.h
+++ multihash/algorithm_identifier.h
@@ -3,14 +3,15 @@
 
 namespace multihash {
 
-enum class algorithm_identifier {
-  identity = 0,
-  sha1 = 0x11,
-  sha2_256 = 0x12,
-  sha2_512 = 0x13,
-  sha3_256 = 0x16
-};
+using algorithm_identifier_type = unsigned int;
 
-using algorithm_identifier_type = std::underlying_type_t<algorithm_identifier>;
+namespace algorithm_identifier {
+constexpr algorithm_identifier_type identity = 0;
+constexpr algorithm_identifier_type sha1 = 0x11;
+constexpr algorithm_identifier_type sha2_256 = 0x12;
+constexpr algorithm_identifier_type sha2_512 = 0x13;
+constexpr algorithm_identifier_type sha3_256 = 0x16;
+
+}  // namespace algorithm_identifier
 
 }  // namespace multihash
